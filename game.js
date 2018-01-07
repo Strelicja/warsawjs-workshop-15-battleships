b3dd6dc0505e841defb8d28bb905f0c1df61e6c2
Strelicja <peritus@iwonajaniak.pl>
2018-01-07 12:35:48 +0100

    add click function

--- game.js
+++ game.js
@@ -1,12 +1,48 @@
 
-const gameContainer = document.querySelector('#game');
-const cell = document.createElement('div');
-const textNode = document.createTextNode ("hello world");
-	cell.appendChild(textNode);
-	gameContainer.appendChild(cell);
+// const gameContainer = document.querySelector('#game');
+// const cell = document.createElement('div');
+// const textNode = document.createTextNode ("hello world");
+// 	cell.appendChild(textNode);
+// 	gameContainer.appendChild(cell);
 
-	let onClick = function onClick() {
-		cell.textContent = "Clicked";
+// 	let onClick = function onClick() {
+// 		cell.textContent = "Clicked";
+// 	}
+// 	cell.addEventListener('click', onClick);
+'use strict';
+
+class Component {
+	getElement() {
+		return this._element;
 	}
-	cell.addEventListener('click', onClick);
-	
+}
+
+class CellComponent extends Component {
+	constructor(){
+		super();
+		this._state = 'unknown';
+		this._element= document.createElement('td');
+		this._element.addEventListener('click', (function () {
+			this.setState('miss');
+		}).bind(this));
+		this._refresh();
+	}
+
+	setState(stateName) {
+		this._state= stateName;
+		this._refresh();
+	}
+	_refresh(){
+		this._element.textContent = this._state;
+		this._element.className = 'cell_' + this._state;
+
+	}
+}
+const myCell = new CellComponent();
+document
+	.getElementById('cellCointainer')
+	.appendChild(myCell.getElement());
+
+
+
+
